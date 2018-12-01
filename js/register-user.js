f198d4cebfc662bb77bee696a061933c8f8bb793
XrosHeart <lucampos2305@gmail.com>
2018-11-30 23:47:31 -0600

    saveUser function

---
+++ js/register-user.js
@@ -0,0 +1,11 @@
+/**
+ * JS relationed with the user's interacts
+ */
+
+ /**
+  * Add an user to localStorage
+  */
+function saveUser() {
+    const user = user();
+    const users = insertToTable('users', user);
+}
