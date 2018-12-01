0444c91e87c8f6e08a8dfc82618b4d0ca127b660
XrosHeart <lucampos2305@gmail.com>
2018-11-30 19:56:51 -0600

    Read of C-R-UD

--- js/data.js
+++ js/data.js
@@ -3,7 +3,8 @@
  */
 
 /**
- * Save the value in the local storage relationated with the key
+ * Save the value in the local storage relationated with the
+ * key
  * @param {*} key the key of the object 
  * @param {*} value the value to be saved
  */
@@ -14,7 +15,8 @@ function saveToLocalStorage(key, value) {
 
 /**
  * Insert a new object into a table located in the localStorage
- * @param {*} tableName name of the table to be inserted in localStorage
+ * @param {*} tableName name of the table to be inserted in 
+ * localStorage
  * @param {*} object object to be inserted in the table
  */
 function insertToTable(tableName, object) {
@@ -29,17 +31,35 @@ function insertToTable(tableName, object) {
     return table;
 }
 
-function deleteFromTable(tableName, objectID){
+/**
+ * Search an object per ID and don't re-write again into the new
+ * table to be returned 
+ * @param {*} tableName name of the table to be modified
+ * @param {*} objectID ID of the object to be deleted
+ */
+function deleteFromTable(tableName, objectID) {
     let table = JSON.parse(localStorage.getItem(tableName));
-    if(!table){
+    if (!table) {
         return false;
     }
     let newTable = [];
     table.array.forEach(element => {
-        if(element.id != objectID){
+        if (element.id != objectID) {
             newTable.push(element);
         }
     });
     localStorage.setItem(tableName, JSON.stringify(newTable));
     return newTable;
+}
+
+/**
+ * Get the data of the table searched
+ * @param {*} tableName name of the table to be obtained
+ */
+function getTableData(tableName){
+    let table = JSON.parse(localStorage.getItem(tableName));
+    if(!table){
+        table = [];
+    }
+    return table;
 }
