1d8b810ee7baa2ef83a9e5e0672294edaf53fcdd
XrosHeart <lucampos2305@gmail.com>
2018-12-08 00:31:46 -0600

    Fixes

--- js/objects.js
+++ js/objects.js
@@ -20,7 +20,7 @@ function userData() {
 /**
  * User login data
  */
-function userLoginData(){
+function userLoginData() {
     let username_log = $.trim($('#username-login').val());
     let password_log = $.trim($('#password-login').val());
     let user_log = {
@@ -33,13 +33,16 @@ function userLoginData(){
 /**
  * Ride add data
  */
-function rideData(){
+function rideData() {
     let ride_id = 0;
     let ride_user = '';
     let ride_descr = $.trim($('#ride_add_descr').val());
     let ride_name = $.trim($('#ride_add_name').val());
     let ride_dep = $.trim($('#ride_add_dep').val());
     let ride_arr = $.trim($('#ride_add_arr').val());
+    let ride_dep_time = $.trim($('#ride_add_dep_time').val());
+    let ride_arr_time = $.trim($('#ride_add_arr_time').val());
+    let ride_days = daysRide();
     let ride_act = true;
     let ride = {
         ride_id,
@@ -48,7 +51,31 @@ function rideData(){
         ride_name,
         ride_dep,
         ride_arr,
+        ride_dep_time,
+        ride_arr_time,
+        ride_days,
         ride_act
     }
     return ride;
+}
+
+function daysRide() {
+    let days = [];
+    $("input[type=checkbox]:checked").each(function () {
+        days.push($(this).prop('id'));
+    });
+    return days;
+}
+
+function loadCheckbox(days) {
+    $('#ride_add_date_M').attr('checked', false);
+    $('#ride_add_date_K').attr('checked', false);
+    $('#ride_add_date_W').attr('checked', false);
+    $('#ride_add_date_T').attr('checked', false);
+    $('#ride_add_date_F').attr('checked', false);
+    $('#ride_add_date_S').attr('checked', false);
+    $('#ride_add_date_Su').attr('checked', false);
+    days.forEach((element, index) => {
+        $(`#${days[index]}`).attr('checked', true);
+    });
 }
--- js/rides.js
+++ js/rides.js
@@ -15,15 +15,22 @@ function saveRide() {
     ride.ride_id = rides.length + 1;
     if (ride.ride_name.length <= 0
         || ride.ride_dep.length <= 0
-        || ride.ride_arr <= 0) {
+        || ride.ride_arr <= 0
+        || ride.ride_dep_time <= 0
+        || ride.ride_arr_time <= 0) {
         popU('No blank spaces');
     } else {
         if (ride.ride_dep === ride.ride_arr) {
             popU('The departure and arrival can\'t called same');
-        } else {
+        } 
+        if(ride.ride_dep_time >= ride.ride_arr_time){
+            popU('The arrival time can\'t be less than the departure time')
+        }
+        else {
             insertToTable('rides', ride);
             document.getElementById('ride-add-form').reset();
             popU('Ride suscesfully added ' + u);
+            renderTableRides('rides_user', 'rides')
         }
     }
 }
@@ -32,13 +39,16 @@ function renderTableRides(htmlObj, tableName) {
     let table = jQuery(`#${htmlObj}_table`);
     let rows = "";
     let rides = getTableData(tableName);
-    let row1 = `<tr><th>Ride Name</th><th>Departure</th><th>Arrival</th><th></th></tr>`
+    let u = uLog('user');
+    let row1 = `<tr><th>Ride Name</th><th>Departure</th><th>Arrival</th><th>Hour</th><th></th></tr>`
     rows += row1;
     rides.forEach(rideEl => {
-        if(rideEl.ride_act){
-            let row2 = `<tr><td>${rideEl.ride_name}</td><td>${rideEl.ride_dep}</td><td>${rideEl.ride_arr}</td>`;
-            row2 += `<td><a onclick="editEntity(this)" data-id="${rideEl.ride_id}" data-entity="${tableName}" class="edit"><i class="material-icons right amber-text">create</i>Edit</a><a  onclick="deleteRide(this, 'rides');" data-id="${rideEl.ride_id}" data-entity="${tableName}" class="delete"><i class="material-icons right red-text">clear</i>Delete</a></td>`
-            rows += row2 + '</tr>';
+        if (rideEl.ride_user == u) {
+            if (rideEl.ride_act) {
+                let row2 = `<tr><td>${rideEl.ride_name}</td><td>${rideEl.ride_dep}</td><td>${rideEl.ride_arr}</td><td>${rideEl.ride_dep_time}</td>`;
+                row2 += `<td><a onclick="editEntity(this)" data-id="${rideEl.ride_id}" data-entity="${tableName}" class="edit"><i class="material-icons right amber-text">create</i>Edit</a><a  onclick="deleteRide(this, 'rides');" data-id="${rideEl.ride_id}" data-entity="${tableName}" class="delete"><i class="material-icons right red-text">clear</i>Delete</a></td>`
+                rows += row2 + '</tr>';
+            }
         }
     });
     table.html(rows);
@@ -53,6 +63,7 @@ function deleteRide(element, tableName) {
     let object = $(element).data();
     let id = object.id;
     changeActive(tableName, id);
+    renderTableRides('rides_user', 'rides');
 }
 
 /**
@@ -60,10 +71,10 @@ function deleteRide(element, tableName) {
  * @param {*} tableName name of table
  * @param {*} id id of the object to be changed
  */
-function changeActive(tableName, id){
+function changeActive(tableName, id) {
     let rides = getTableData(tableName);
     rides.forEach(ele => {
-        if(ele.ride_id == id){
+        if (ele.ride_id == id) {
             ele.ride_act = false;
         }
     });
@@ -75,9 +86,10 @@ function changeActive(tableName, id){
  * Modals function
  */
 $(document).ready(function () {
-    $('#modal-add-ride').modal({
+    $('#modal-main-add-ride').modal({
         opacity: 0.6
     });
+    $('.timepicker').timepicker();
 });
 
 /**
